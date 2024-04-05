b40634414cf337bd1f30e238b0da363f7a391ed3
Victor Matheus <victormatheus507@gmail.com>
2024-04-05 11:08:01 -0300

    Criando contexto de transacoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { Transactions } from "./pages/Transactions";
-import { GlobalStyled } from "./styles/global";
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { Transactions } from './pages/Transactions';
+import { GlobalStyled } from './styles/global';
+import { TransactionsProvider } from './contexts/TransactionsContext';
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyled/>
-
-      <Transactions/>
+      <GlobalStyled />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
-  )
+  );
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary/index.';
 import { SearchForm } from './Components/SearchForm';
@@ -7,29 +7,10 @@ import {
   TransationsContainer,
   TransationsTable,
 } from './styles';
-
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions');
-    const data = await response.json();
-
-    setTransactions(data);
-  }
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <div>
